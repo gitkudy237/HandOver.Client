f25e20a4aad70944a4deaa06686ef64c4f30d26d
kudy <bayardkudy1@gmail.com>
2025-01-29 13:23:11 +0100

    Add ItemsConditionsClient client

---
+++ Clients/ItemsConditionsClient.cs
@@ -0,0 +1,18 @@
+using System;
+using HandOver.Client.Models;
+
+namespace HandOver.Client.Clients;
+
+public class ItemsConditionsClient
+{
+    private readonly ItemCondition[] _conditions =
+    [
+        new() {Id = 1, Condition = "New"},
+        new() {Id = 2, Condition = "Like new"},
+        new() {Id = 3, Condition = "Fairly used"},
+        new() {Id = 4, Condition = "Heavily used"},
+    ];
+
+    public ItemCondition[] GetItemsConditions()
+        => _conditions;
+}
