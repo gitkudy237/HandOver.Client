f7138698d2c8e053260df24a47e823940d7b7c63
kudy <bayardkudy1@gmail.com>
2025-02-11 02:47:02 +0100

    Add client-side validation

--- Models/CreateItem.cs
+++ Models/CreateItem.cs
@@ -1,13 +1,24 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HandOver.Client.Models;
 
 public class CreateItem
 {
+    [Required(ErrorMessage = "Item is required")]
+    [StringLength(30, ErrorMessage = "Name should be at most 30 characters")]
     public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Please select a category")]
     public int CategoryId { get; set; }
+
+    [Required(ErrorMessage = "Item condition is required")]
     public int ItemConditionId { get; set; }
+
+    [Required][StringLength(20)]
     public string? Location { get; set; }
+
+    [Required][Range(500, 1_000_000)]
     public int Price { get; set; }
     public string? Description { get; set; }
     public string? ImageUrl { get; set; }
