8e756d0b540f7dc5ed2b4bf76bb5acfaec39a8d2
kudy <bayardkudy1@gmail.com>
2025-02-11 02:14:29 +0100

    Fix the update bug

--- Models/CreateItem.cs
+++ Models/CreateItem.cs
@@ -4,11 +4,11 @@ namespace HandOver.Client.Models;
 
 public class CreateItem
 {
-    public required string Name { get; set; }
+    public string? Name { get; set; }
     public int CategoryId { get; set; }
     public int ItemConditionId { get; set; }
-    public required string Location { get; set; }
+    public string? Location { get; set; }
     public int Price { get; set; }
     public string? Description { get; set; }
-    public required string ImageUrl { get; set; }
+    public string? ImageUrl { get; set; }
 }
