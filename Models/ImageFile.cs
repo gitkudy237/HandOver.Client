b38c851c48954ebdefb92e4c520620b333b8d38f
kudy <bayardkudy1@gmail.com>
2025-02-14 01:05:49 +0100

    Add ImageFile model

---
+++ Models/ImageFile.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HandOver.Client.Models;
+
+public class ImageFile
+{
+    public string? Base64Data { get; set; }
+    public string? ContentType { get; set; }
+    public string? FileName { get; set; }
+}
