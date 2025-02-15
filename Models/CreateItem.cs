8fc284b6c30bdadda22c407617eedabeaf1cc391
kudy <bayardkudy1@gmail.com>
2025-02-15 02:04:41 +0100

    Upload image files

--- Models/CreateItem.cs
+++ Models/CreateItem.cs
@@ -22,4 +22,5 @@ public class CreateItem
     public int Price { get; set; }
     public string? Description { get; set; }
     public string? ImageUrl { get; set; }
+    public List<ImageFile>? Images { get; set; }
 }
