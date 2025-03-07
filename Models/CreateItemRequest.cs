b08f069ab547a7dd4b892a096f8ca3f71d56c17f
kudy <bayardkudy1@gmail.com>
2025-03-07 21:20:59 +0100

    Create Announcement component and Item request model

---
+++ Models/CreateItemRequest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HandOver.Client.Models;
+
+public class CreateItemRequest
+{
+    [Required][StringLength(30)]
+    public string? Name { get; set; }
+    public int CategoryId { get; set; }
+    public string? Description { get; set; }
+    public ImageFile? Image { get; set; }
+
+    [Required][StringLength(20)]
+    public string? Location { get; set; }
+
+    [Required][StringLength(9)]
+    public string? UserContact { get; set; }
+
+    [Range(500, 999_999)]
+    public int MinPrice { get; set; }
+    
+    [Range(501, 1_000_000)]
+    public int MaxPrice { get; set; }
+}
