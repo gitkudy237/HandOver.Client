81007e6f651e0ca623bbf05891fd440ab6299489
kudy <bayardkudy1@gmail.com>
2025-03-23 18:08:16 +0100

    Create announcement modal

--- Clients/AnnouncementsClient.cs
+++ Clients/AnnouncementsClient.cs
@@ -9,6 +9,9 @@ public class AnnouncementsClient(HttpClient httpClient)
     public async Task<AnnouncementSummary[]> GetAnnouncementsAsync()
         => await httpClient.GetFromJsonAsync<AnnouncementSummary[]>("announcements") ?? [];
     
+    public async Task<AnnouncementSummary?> GetAnnouncementByIdAsync(int id)
+        => await httpClient.GetFromJsonAsync<AnnouncementSummary>($"announcements/{id}");
+    
     public async Task AddAnnouncementAsync(CreateItemRequest announcement)
         => await httpClient.PostAsJsonAsync("announcements", announcement);
 
